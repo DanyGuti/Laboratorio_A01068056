7461ec1a9affd8f90458010aa24f8c20c1edf7ef
Leslie <A01708987@tec.mx>
2022-05-12 10:25:24 -0500

    Funciones anadidas

--- Programa.c
+++ Programa.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+
+int suma(int a, int b){ 
+    return a+b; 
+} 
+int resta(int a, int b){ 
+    return a-b; 
+} 
+
+
 int main(){
     int length;
     scanf("%i", &length);
